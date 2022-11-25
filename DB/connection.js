7237a07e5d1b440b13f3111bd556ad69ef31f5cc
tariq-shreem <tariqshreem00@gmail.com>
2022-11-25 12:52:54 +0200

    first

---
+++ DB/connection.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectDB = async ()=>{
+
+    return await mongoose.connect("mongodb+srv://tariq:tariq55@cluster0.yesyf.mongodb.net/saraha")
+    .then( result=>{
+        console.log(`connectionDB `);
+    }).catch( err=>{
+        console.log(`error to connect db`,err);
+    } );
+}
+
+export default connectDB;
